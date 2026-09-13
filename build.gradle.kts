ba6fe4ad3ff7ee8591e4224d014f80a682905545
Soheil VatanDoust <vatandoust.soheil@gmail.com>
2026-09-13 12:17:35 +0330

    fix(build): resolve Gradle plugin DSL syntax errors in root and module build scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    id("com.android.application") version "8.8.2" apply false
+    id("com.android.library") version "8.8.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
 }
